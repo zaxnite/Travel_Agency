0981bd6f0da699f9a71c60ddbece8e11e21b5ffc
Sandra-Raj <89869306+Sandra-Raj@users.noreply.github.com>
2023-04-30 20:02:40 +0400

    form updated

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,76 @@
+// import React, { useState } from 'react';
+
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll",function(){
-    header.classList.toggle ("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 100);
 })
+
+let menu = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navbar.classList.toggle('open')
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" activepic", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " activepic";
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("activeanim");
+    } else {
+        reveals[i].classList.remove("activeanim");
+    }
+    }
+}
+window.addEventListener("scroll", reveal);
+
+// function TextInput({ type = 'text', label }) {
+//     const [value, setValue] = useState('');
+
+//     function handleChange(e) {
+//     setValue(e.target.value);
+//     }
+
+//     return (
+//     <div className="input-container">
+//         <input type={type} value={value} onChange={handleChange} />
+//         <label className={value && 'filled'} htmlFor={name}>
+//         {label}
+//         </label>
+//     </div>
+//     );
+// }
---
+++ book.php
@@ -0,0 +1,22 @@
+<?php
+    $username = $_POST['name'];
+    $countryfrom = $_POST['from'];
+    $countryto = $_POST['to'];
+    $passengers = $_POST['passengers'];
+    $departure = $_POST['departure'];
+    $return = $_POST['return'];
+
+    $conn = new mysqli('localhost','root','','flights_booking');
+    if($conn->connect_error){
+        die('Connection failed : '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into flight_booking(username, country_from, country_to, passengers, departure, arrival) 
+        values(?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssiss",$username, $countryfrom, $countryto, $passengers, $departure, $return);
+        $stmt->execute();
+        echo "Booked successfully";
+        $stmt->close();
+        $conn->close();
+    }
+?>
+
