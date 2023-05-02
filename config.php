dba2f164c5289f9b4f5726e5af5f89f4535b4d62
Sandra-Raj <89869306+Sandra-Raj@users.noreply.github.com>
2023-05-02 22:03:02 +0400

    form

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'support_queries');
+define('DB_NAME', 'travel_agency');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
